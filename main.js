429803c431d4a500ca19db30310a9d1724d0b84d
Macbook-air <40375223+fhernandez2583@users.noreply.github.com>
2025-03-27 09:46:55 -0500

    complete total profit

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 // Create variance for initial cost
 const initialCost = 100;
 
+// Create Discount Rate
+const discountRate = 0.045;
+
+// Create Period
+const period = 5;
+
 // Table arrays
 const projectRevenue = [2, 3, 4, 5, 6];
-const projectCost = [5, 4, 3, 2, 1];
+const projectCost = [5, 3, 7, 2, 1];
 const totalProfit = (arr1, arr2) => {
     let sum = [];
     for(let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
         sum.push(arr1[i] + arr2[i]);
-    } return sum;
+    } return sum = sum.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+      }, 0);
 };
 console.log(totalProfit(projectRevenue, projectCost));
