b2a89681cc094376bec351dfc81d2d605bfe8fd9
Macbook-air <40375223+fhernandez2583@users.noreply.github.com>
2025-03-27 09:34:48 -0500

    declare variables and function for total project for cashflow

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 const initialCost = 100;
 
 // Table arrays
-const projectRevenue = [];
-const projectCost = [];
+const projectRevenue = [2, 3, 4, 5, 6];
+const projectCost = [5, 4, 3, 2, 1];
 const totalProfit = (arr1, arr2) => {
-    
+    let sum = [];
+    for(let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+        sum.push(arr1[i] + arr2[i]);
+    } return sum;
 };
-
+console.log(totalProfit(projectRevenue, projectCost));
