cf891bd5c49ff9d1187f66ca407ab08dd4128e0c
Macbook-air <40375223+fhernandez2583@users.noreply.github.com>
2025-03-27 10:55:49 -0500

    refactored npv formula

--- main.js
+++ main.js
@@ -2,39 +2,48 @@
 const initialCost = -100000;
 
 // Create Discount Rate
-const discountRate = 0.045;
-
+// const discountRate = 0.045;
 // Create Period
-const period = 5;
+// const period = 5;
 
 // Table arrays
 const projectRevenue = [30000, 30000, 30000, 30000, 30000];
 const projectCost = [-15000, -15000, -15000, -15000, -15000];
+
 const totalProfit = (arr1, arr2) => {
     let sum = [];
+    let discountRate = 0.045;
+    let period = [1, 2, 3, 4, 5];
+
     for(let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
         sum.push(arr1[i] + arr2[i]);
-    } return sum = sum.reduce((accumulator, currentValue) => {
+    }
+    let = sumOfDiscount = [];
+
+    for(let i = 0; i < Math.max(sum.length, period.length); i++) {
+        sumOfDiscount.push(sum[i] / Math.pow((1 + discountRate), period[i]));
+    }
+    return sumOfDiscount = sumOfDiscount.reduce((accumulator, currentValue) => {
         return accumulator + currentValue
-      }, 0);
+      }, 0) + initialCost;
 };
-// console.log(totalProfit(projectRevenue, projectCost));
+console.log(totalProfit(projectRevenue, projectCost));
 
 // Create NPV Denominator
-const npvDenominator = (discountRate, period) => {
-    return Math.pow((1 + discountRate), period);
-}
-console.log(npvDenominator(discountRate, period));
+// const npvDenominator = (discountRate, period) => {
+//     return Math.pow((1 + discountRate), period);
+// }
+// console.log(npvDenominator(discountRate, period));
 
 // Create Discounted amount
-const discountedCashFlow = (totalProfit, npvDenominator) => {
-    return totalProfit / npvDenominator;
-}
-console.log(discountedCashFlow(totalProfit(projectRevenue, projectCost), npvDenominator(discountRate, period)));
+// const discountedCashFlow = (totalProfit, npvDenominator) => {
+//     return totalProfit / npvDenominator;
+// }
+// console.log(discountedCashFlow(totalProfit(projectRevenue, projectCost), npvDenominator(discountRate, period)));
 
 
 // Create NPV function
-const npv = (initialCost, discountedCashFlow) => {
-    return initialCost + discountedCashFlow;
-}
-console.log(npv(initialCost, discountedCashFlow(totalProfit(projectRevenue, projectCost), npvDenominator(discountRate, period))));
+// const npv = (initialCost, discountedCashFlow) => {
+//     return initialCost + discountedCashFlow;
+// }
+// console.log(npv(initialCost, discountedCashFlow(totalProfit(projectRevenue, projectCost), npvDenominator(discountRate, period))));
