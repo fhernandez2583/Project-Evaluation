4acc7ac282fadf95bd7cc5bd51e73048ba3dd950
Francisco <40375223+fhernandez2583@users.noreply.github.com>
2025-03-27 09:13:25 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Create variance for initial cost
+const initialCost = 100;
+
+// Table arrays
+const projectRevenue = [];
+const projectCost = [];
+const totalProfit = (arr1, arr2) => {
+    
+};
+
