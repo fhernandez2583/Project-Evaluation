675ea8f10bd67cc4c04c78575fa49a9a1200704e
Macbook-air <40375223+fhernandez2583@users.noreply.github.com>
2025-03-31 15:03:26 -0500

    main.js and index.html connected

--- main.js
+++ main.js
@@ -1,63 +1,27 @@
-// Find a check to always make cost negative and revenue positive
-// Rename function to projectRevenue
-const revenuePos = (arr) => {
-  let checkedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < 0) {
-      checkedArr[i] = arr[i] * -1;
-    } else {
-      checkedArr[i] = arr[i];
-    }
-  }
-  return checkedArr;
+// HTML Elements
+// let numYearsInput;
+let projectRevenue;
+let projectCost
+let initialCost;
+
+document.getElementById("submit").onclick = function () {
+  // numYearsInput = parseInt(document.getElementById("numYearsInput").value);
+  projectRevenue = [parseInt(document.getElementById("revYear1").value), parseInt(document.getElementById("revYear2").value), parseInt(document.getElementById("revYear3").value), parseInt(document.getElementById("revYear4").value), parseInt(document.getElementById("revYear5").value)];
+  projectCost = [parseInt(document.getElementById("costYear1").value), parseInt(document.getElementById("costYear2").value), parseInt(document.getElementById("costYear3").value), parseInt(document.getElementById("costYear4").value), parseInt(document.getElementById("costYear5").value)];
+  initialCost = parseInt(document.getElementById("initialCost").value);
+
+  document.getElementById("npvResults").textContent = `NPV is: ${npvFormula(projectRevenue, projectCost)}`;
+  document.getElementById("mIrrResults").textContent = `Modified IRR is: ${mIrrFormula(projectRevenue, projectCost)}`;
 };
-// console.log(revenuePos([-10000, -10000, 5000, -5000, -2000]));
-
-// Rename function to projectCost
-const costNegative = (arr) => {
-  let checkedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      checkedArr[i] = arr[i] * -1;
-    } else {
-      checkedArr[i] = arr[i];
-    }
-  }
-  return checkedArr;
-};
-// console.log(costNegative([10000, -10000, 5000, 5000, -2000]));
-
-// Rename function to initialCost
-const initialCostNegative = (cost) => {
-  if (cost > 0) {
-    return cost * -1;
-  } else {
-    return cost;
-  }
-};
-// console.log(initialCostNegative(-100000));
-
-// Create variance for initial cost
-const initialCost = -50000;
-
-// Create Discount Rate
-// const discountRate = 0.045;
-// Create Period
-// const period = 5;
-
-// Table arrays
-const projectRevenue = [30000, 30000, 30000, 0, 0];
-const projectCost = [-15000, -15000, -15000, 0, 0];
-const projectNumYears = 3;
 
 // NPV Formula
-const npvFormula = (arr1, arr2) => {
+const npvFormula = (a, b) => {
   let sum = [];
   let discountRate = 0.045;
   let period = [1, 2, 3, 4, 5];
 
-  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
-    sum.push(arr1[i] + arr2[i]);
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    sum.push(a[i] + b[i]);
   }
   let = sumOfDiscount = [];
 
@@ -70,39 +34,32 @@ const npvFormula = (arr1, arr2) => {
       return accumulator + currentValue;
     }, 0) + initialCost);
 };
-console.log(npvFormula(projectRevenue, projectCost));
 
 // Modified IRR Formula added
-const mIrrFormula = (arr1, arr2) => {
+const mIrrFormula = (a, b) => {
   let sum = [];
   let reinvestRate = 0.02;
   let period = [1, 2, 3, 4, 5];
-  let yearCheck = projectNumYears;
+  // let yearCheck = numYearsInput;
 
-  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
-    sum.push(arr1[i] + arr2[i]);
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    sum.push(a[i] + b[i]);
   }
 
   let = sumOfReinvest = [];
 
-  for (let i = 0; i < Math.max(sum.length - yearCheck, period.length - yearCheck); i++) {
+  for (let i = 0; i < Math.max(sum.length - 1, period.length - 1); i++) {
     sumOfReinvest.push(sum[i] * Math.pow(1 + reinvestRate, period[i]));
   }
-  
-  console.log(sumOfReinvest);
 
   sumOfReinvest.push(sum.pop());
-
-console.log(sumOfReinvest);
 
   sumOfReinvest = (
     sumOfReinvest.reduce((accumulator, currentValue) => {
       return accumulator + currentValue;
     }, 0) / (initialCost * -1)) ;
 
-let expValue = sumOfReinvest ** (1/projectNumYears);
+let expValue = sumOfReinvest ** (1/5);
 
   return expValue - 1;
 };
-console.log(mIrrFormula(projectRevenue, projectCost));
-// Will revisit formulas when I check input from html
